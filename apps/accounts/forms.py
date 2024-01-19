9a4acaa47ea90a3d8aff6316c969794a017f3499
atelhores <lologan228@yandex.ru>
2024-01-19 19:33:17 +1000

    error msg + email

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,12 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = (
             'username',
+            'email',
             'password1',
             'password2',
         )
